1a052eb4caa701b5d574e5a5fa03a77e9cf8f230
Nancy <Nancy@Nancy-PC>
2016-04-10 22:21:28 -0500

    PROBANDOOOO INDEX TÍTULO

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     
     <meta charset="utf-8">
-    <title>Vicerrectoria Asistente de Investigaci&oacute;n y Extensi&oacute;n</title>
+    <title>VAIE Vicerrectoria Asistente de Investigaci&oacute;n y Extensi&oacute;n</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
     <meta name="author" content="Muhammad Usman">
